a6cea3b29b07783112fbe18541fc6771adc9092f
Leon Hsu <leonhsu95@gmail.com>
2021-05-17 12:42:31 +1000

    Function to Get Data as manually typed in db.json

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-// Dependencies
+// DEPENDENCIES
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,14 +10,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-// Sets up the Express app to handle data parsing
+// SETS UP EXPRESS TO HANDLE DIRECTORY PARSING
 app.use(express.static("public"));
 
-//LISTENERS
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+// LISTENERS
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 
-app.listen(PORT, () => {
-    console.log(`App listening on http://localhost:${PORT}`);
+// API GET REQUEST TO RETRIEVE DATA SAVED from ./db/db.json
+app.get("/api/notes", (req, res) => {
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(data);
 });
+
+app.listen(PORT, () => {console.log(`App listening on http://localhost:${PORT}`);});
+
